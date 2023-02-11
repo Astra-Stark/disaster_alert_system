02d939c3968f494cb8537f39320e075a623fc4ff
Astra-Stark <astha27sharma@gmail.com>
2023-02-12 04:06:48 +0530

    First commit

---
+++ ML.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import warnings
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import model_selection
+import math
+import numpy as np
+warnings.filterwarnings("ignore")
+risk_data=pd.read_csv('hh.csv')
+# risk_data['Date'] = pd.to_datetime(risk_data['Date'],utc=True)
+# risk_data['Date'] =pd.to_datetime(risk_data['Date']).dt.date
+risk_data['Date'] = risk_data['Date'].str.replace('-', '')
+risk_data['Date'] = risk_data['Date'].str.replace(':', '')
+risk_data['Date'] = risk_data['Date'].str.replace('/', '')
+risk_data['Date'] = risk_data['Date'].str.replace(' ', '')
+print(risk_data[risk_data['Date'].isnull()])
+risk_data['Date'] = pd.to_numeric(risk_data['Date'], errors='coerce')
+risk_data= risk_data.dropna(subset=['Date'])
+risk_data['Date'] = risk_data['Date'].astype(int)
+risk_data=risk_data.head(20)
+X=risk_data.drop('Magnitude',axis='columns')
+Y=risk_data.Magnitude
+X=np.array(X)
+Y=np.array(Y)
+X_train, X_test, Y_train, Y_test=train_test_split(X,Y,random_state=10)
+model = LinearRegression()
+model.fit(X_train, Y_train)
+y_pred = model.predict([[1172022,134,140.2,22]])
+print(y_pred)
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, url_for, redirect, render_template
+import pickle
+import numpy as np
+app = Flask(__name__, template_folder='template',
+             static_folder='staticFiles')
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def hello():
+        return render_template("index.html")
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+        int_features = [float(x) for x in request.form.values()]
+        final = [np.array(int_features)]
+        prediction = model.predict(final)
+        if prediction >= 3:
+            return render_template('index.html', pred='Risky')
+        else:
+            return render_template('index.html', pred='safe')
+if __name__ == "__main__":
+        app.run(host='localhost', port=5000)
