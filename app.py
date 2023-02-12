8d3e476fa03ad90abad75bb60f1def00a28a21d6
Janvi <janvimalhotra981@gmail.com>
2023-02-12 06:06:47 +0530

    commit-2

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def hello():
         return render_template("index1.html")
+@app.route('/flood', methods=['POST','GET'])
+def floods():
+        return render_template('floodprediction.html')
 @app.route('/earthquake', methods=['POST','GET'])
 def earthquake():
         return render_template('index.html')
@@ -19,5 +22,16 @@ def predict():
             return render_template('index.html', pred='Risky')
         else:
             return render_template('index.html', pred='safe')
+
+
+# @app.route('/predicts', methods=['POST', 'GET'])
+# def predicts():
+#         int_features = [float(x) for x in request.form.values()]
+#         final = [np.array(int_features)]
+#         prediction = model.predict(final)
+#         if prediction == 1:
+#             return render_template('floodprediction.html', pred='Risky')
+#         else:
+#             return render_template('floodprediction.html', pred='safe')
 if __name__ == "__main__":
         app.run(host='localhost', port=5000)
