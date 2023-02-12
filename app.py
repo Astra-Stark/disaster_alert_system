e277aa4a091f7c376a68df81ab958cb6adf12206
Janvi <janvimalhotra981@gmail.com>
2023-02-12 07:30:44 +0530

    commit-4

--- app.py
+++ app.py
@@ -7,9 +7,31 @@ model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def hello():
         return render_template("index1.html")
+@app.route('/hailstrom', methods=['POST','GET'])
+def hailstorm():
+        return render_template('hailstrom.html')
+@app.route('/pre', methods=['POST', 'GET'])
+def predicts():
+        int_features = [float(x) for x in request.form.values()]
+        final = [np.array(int_features)]
+        prediction = model.predict(final)
+        if prediction == 1:
+            return render_template('hailstrom.html', pred='Risky')
+        else:
+            return render_template('hailstrom.html', pred='safe')
+        
 @app.route('/flood', methods=['POST','GET'])
 def floods():
         return render_template('floodprediction.html')
+@app.route('/predicts', methods=['POST', 'GET'])
+def predicts():
+        int_features = [float(x) for x in request.form.values()]
+        final = [np.array(int_features)]
+        prediction = model.predict(final)
+        if prediction == 1:
+            return render_template('floodprediction.html', pred='Risky')
+        else:
+            return render_template('floodprediction.html', pred='safe')
 @app.route('/earthquake', methods=['POST','GET'])
 def earthquake():
         return render_template('index.html')
@@ -23,15 +45,5 @@ def predict():
         else:
             return render_template('index.html', pred='safe')
 
-
-# @app.route('/predicts', methods=['POST', 'GET'])
-# def predicts():
-#         int_features = [float(x) for x in request.form.values()]
-#         final = [np.array(int_features)]
-#         prediction = model.predict(final)
-#         if prediction == 1:
-#             return render_template('floodprediction.html', pred='Risky')
-#         else:
-#             return render_template('floodprediction.html', pred='safe')
 if __name__ == "__main__":
         app.run(host='localhost', port=5000)
